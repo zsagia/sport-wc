c4035525f2322f6a5a5e74735b11adbe4f08f0c0
Zsolt Szabo <zsolt.szabo@liferay.com>
2015-09-11 08:22:27 +0200

    #8 Add person model

---
+++ src/model/person.js
@@ -0,0 +1,15 @@
+function Person(data) {
+	this._id = data.id;
+	this._first_name = data.first_name;
+	this._second_name = data.second_name;
+	this._nick_name = data.nick_name;
+	this._height = data.height;
+	this._weight = data.weight;
+	this._birth_place = data.birth_place;
+	this._birth_date = data.birth_date;
+	this._nationality = data.nationality;
+}
+
+Person.prototype.toString = function() {
+	return this._first_name + (this._nick_name : this._nick_name ? '') + this._second_name;
+};
