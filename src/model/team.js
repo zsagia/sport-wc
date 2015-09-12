e70829523807498ddc7c6536ec939756119a3069
Zsolt Szabo <zsolt.szabo@liferay.com>
2015-09-12 16:52:11 +0200

    #2 Added new team attributes

--- src/model/team.js
+++ src/model/team.js
@@ -1,8 +1,12 @@
 function Team(data) {
 	this._id = data.id;
+	this._established = data.established;
 	this._logo = data.logo;
+	this._manager = data.manager;
 	this._name = data.name;
+	this._nick_name = data.nick_name;
 	this._short_name = data.short_name;
+	this._site = data.site;
 }
 
 Team.prototype.toString = function() {
