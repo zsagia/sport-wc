1e1f01e4727912e68bc269d2d0a04a00d48180e1
Zsolt Szabo <zsolt.szabo@liferay.com>
2015-09-13 19:53:26 +0200

    #2 Add team model.

---
+++ src/model/team.js
@@ -0,0 +1,12 @@
+function Team(data) {
+	this._id = data.id;
+	this._club = data.club;
+	this._name = data.name;
+	this._players = data.players;
+	this._trainers = data.trainers;
+	this._season = data.season
+}
+
+Team.prototype.toString = function() {
+	return this._name;
+};
