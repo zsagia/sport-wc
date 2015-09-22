94d1fb780dbbd361bcfe46fe80d5770f5b9f71e6
Zsolt Szabo <zsolt.szabo@liferay.com>
2015-09-22 19:14:53 +0200

    #12 Add venues mock data

---
+++ demos/venue/mock/venues.js
@@ -0,0 +1,148 @@
+var venues = [
+	{
+		"id": 6001,
+		"built": 1909,
+		"country": "UK",
+		"image": "http://www.premierleague.com/content/dam/premierleague/shared-images/clubs/m/man-utd/hero9.jpg/_jcr_content/renditions/original",
+		"name": "Old Trafford",
+		"city": "Manchester",
+		"capacity": 75653
+	},
+	{
+		"id": 6002,
+		"built": 2006,
+		"country": "UK",
+		"image": "http://www.premierleague.com/content/dam/premierleague/shared-images/clubs/a/arsenal/hero7.jpg/_jcr_content/renditions/original",
+		"name": "Emirates",
+		"city": "London",
+		"capacity": 60260
+	},
+	{
+		"id": 6003,
+		"built": 1897,
+		"country": "UK",
+		"name": "Villa Park",
+		"city": "Birmingham",
+		"capacity": 42660
+	},
+	{
+		"id": 6004,
+		"built": 1910,
+		"country": "UK",
+		"name": "Vitality",
+		"city": "Bournemouth",
+		"capacity": 11464
+	},
+	{
+		"id": 6005,
+		"built": 1877,
+		"name": "Stamford Bridge",
+		"city": "London",
+		"capacity": 41798
+	},
+	{
+		"id": 6006,
+		"built": 1924,
+		"name": "Selhurst Park",
+		"city": "London",
+		"capacity": 25073
+	},
+	{
+		"id": 6007,
+		"built": 1892,
+		"name": "Goodison Park",
+		"city": "Liverpool",
+		"capacity": 39571
+	},
+	{
+		"id": 6008,
+		"built": 2002,
+		"name": "King Power",
+		"city": "Leicester",
+		"capacity": 32312 
+	},
+	{
+		"id": 6009,
+		"built": 1884,
+		"name": "Anfield",
+		"city": "Liverpool",
+		"capacity": 44742
+	},
+	{
+		"id": 60010,
+		"built": 2002,
+		"name": "Etihad",
+		"city": "Manchester",
+		"capacity": 55097
+	},
+	{
+		"id": 6011,
+		"built": 1892,
+		"name": "St James' Park",
+		"city": "Newcastle",
+		"capacity": 52338
+	},
+	{
+		"id": 6012,
+		"built": 1935,
+		"name": "Carrow Road",
+		"city": "Norwich",
+		"capacity": 27010
+	},
+	{
+		"id": 6013,
+		"built": 2001,
+		"name": "St. Mary's",
+		"city": "Southampton",
+		"capacity": 32505
+	},
+	{
+		"id": 6014,
+		"built": 1997,
+		"name": "Britannia",
+		"city": "Stoke-on-Trent",
+		"capacity": 27740
+	},
+	{
+		"id": 6015,
+		"built": 1997,
+		"name": "Stadium of Light",
+		"city": "Sunderland",
+		"capacity": 48707
+	},
+	{
+		"id": 6016,
+		"built": 2005,
+		"name": "Liberty Stadium",
+		"city": "Swansea",
+		"capacity": 20909
+	},
+	{
+		"id": 6017,
+		"built": 1889,
+		"name": "White Hart Lane",
+		"city": "London",
+		"capacity": 36284
+	},
+	{
+		"id": 6018,
+		"built": 1922,
+		"name": "Vicarage Road Stadium",
+		"city": "Watford",
+		"capacity": 21500
+	},
+	{
+		"id": 6019,
+		"built": 1900,
+		"name": "The Hawthorns",
+		"city": "West Midlands",
+		"capacity": 26850
+	},
+	{
+		"id": 6020,
+		"built": 1904,
+		"name": "Boleyn Ground",
+		"city": "London",
+		"capacity": 35345
+	}
+]
