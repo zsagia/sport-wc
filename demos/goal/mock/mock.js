15b9ba4c6472cbc02faaf53993c33c47395f90e0
Zsolt Szabo <zsolt.szabo@liferay.com>
2015-10-08 21:14:27 +0200

    #16 Goal test

---
+++ demos/goal/mock/mock.js
@@ -0,0 +1,32 @@
+players = [
+	{
+		id: 1003,
+		name: 'Philippe Coutinho',
+		team: 1004
+	},
+	{
+		id: 1006,
+		name: 'Marouane Fellaini',
+		team: 1005
+	}
+];
+
+var matches = [
+	{
+		id: 3101,
+		attendance: 75261,
+		awayTeam: 'Manchester United',
+		awayScores: 0,
+		homeTeam: 'Liverpool FC',
+		homeScores: 1,
+		date: 1439041500000,
+		round: 1,
+		season: '2015\/2016',
+		venue: 6001,
+		type: 'Premiere League'
+	}
+];
+
+goals = [
+	{id: 2001,date: 10,player: {id: 1003,name: 'Philippe Coutinho',team: 1004},match: {id: 3101,attendance: 75261,awayTeam: 2003,awayScores: 0,homeTeam: 2002,homeScores: 1,date: 1439041500000,round: 1,season: '2015\/2016',venue: 6001,type: 'Premiere League'}}
+];
---
+++ spec/GoalSpec.js
@@ -0,0 +1,67 @@
+describe("Goal instance", function() {
+	var goal;
+
+	var player = {
+		id: 12345,
+		birth_date: 598492800000,
+		birth_place: 'Chile',
+		first_name: 'Alexis',
+		height: 168,
+		nationality: 'Chile',
+		number: 17,
+		position: 'forward',
+		second_name: 'Sánches',
+		weight: 62
+	};
+
+	var match = {
+		id: 1002,
+		attendance: 40000,
+		away_goals: 1,
+		away_team: 'Liverpool',
+		date: 1440757875,
+		home_goals: 1,
+		home_team: 'Tottenham',
+		type: 'friendly',
+		venue: 'london'
+	};
+
+	var data = {
+		id: 34567,
+		match: match,
+		player: player,
+		date: 1442312355
+	};
+
+	beforeEach(function() {
+		goal = new Goal(data);
+	});
+
+	it("date should be equal", function() {
+		expect(goal.date).toEqual(1442312355);
+	});
+
+	it("player should not be null", function() {
+		expect(goal.player).not.toBeNull();
+	});
+
+	it("player should not be undefined", function() {
+		expect(goal.player).not.toBeUndefined();
+	});
+
+	it("player should be an Object", function() {
+		expect(typeof goal.player === 'object').toEqual(true);
+	});
+
+	it("match should not be null", function() {
+		expect(goal.match).not.toBeNull();;
+	});
+
+	it("match should not be undefined", function() {
+		expect(goal.match).not.toBeUndefined();
+	});
+
+	it("match should be an Object", function() {
+		expect(typeof goal.match === 'object').toEqual(true);
+	});
+});
