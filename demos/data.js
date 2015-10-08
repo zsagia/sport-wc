33ab108742821f9f3df46bb1e69770aed851e920
Zsolt Szabo <zsolt.szabo@liferay.com>
2015-10-08 21:30:13 +0200

    #19 Add a global mock data

---
+++ demos/data.js
@@ -0,0 +1,431 @@
+window.matches = [
+	{
+		id: 7101,
+		attendance: 75261,
+		away_team: 2003,
+		away_goals: 0,
+		home_team: 2002,
+		home_goals: 1,
+		date: 1439041500000,
+		round: 1,
+		season: '2015\/2016',
+		venue: 6001,
+		type: 'Premiere League'
+	},	
+	{
+		id: 7102,
+		attendance:  11155,
+		away_team: 2006,
+		away_goals: 1,
+		home_team: 2004,
+		home_goals: 0,
+		date: 1439046000000,
+		round: 1,
+		season: '2015\/2016',
+		venue: 6004,
+		type: 'Premiere League'
+	},
+	{
+		id: 7103,
+		attendance: 27036,
+		away_team: 2008,
+		away_goals: 3,
+		home_team: 2013,
+		home_goals: 1,
+		date: 1439046000000,
+		round: 1,
+		referee: 'Simon Hooper',
+		season: '2015\/2016',
+		venue: 6012,
+		type: 'Premiere League'
+	},
+	{
+		id: 7104,
+		attendance: 39063,
+		away_team: 2018,
+		away_goals: 2,
+		home_team: 2009,
+		home_goals: 2,
+		date: 1439046000000,
+		round: 1,
+		referee: 'Mike Jones',
+		season: '2015\/2016',
+		venue: 6007,
+		type: 'Premiere League'
+	},
+	{
+		id: 7105,
+		attendance: 32242,
+		away_team: 2016,
+		away_goals: 2,
+		home_team: 2010,
+		home_goals: 4,
+		date: 1439046000000,
+		round: 1,
+		referee: 'Lee Mason',
+		season: '2015\/2016',
+		venue: 6008,
+		type: 'Premiere League'
+	},
+	{
+		id: 7106,
+		attendance: 41232,
+		away_team: 2017,
+		away_goals: 2,
+		home_team: 2007,
+		home_goals: 2,
+		date: 1439055000000,
+		round: 1,
+		referee: 'Michael Oliver',
+		season: '2015\/2016',
+		venue: 6005,
+		type: 'Premiere League'
+	},
+	{
+		id: 7107,
+		attendance: 59996,
+		away_team: 2020,
+		away_goals: 2,
+		home_team: 2005,
+		home_goals: 0,
+		date: 1439127000000,
+		round: 1,
+		referee: 'Martin Atkinson',
+		season: '2015\/2016',
+		venue: 6002,
+		type: 'Premiere League'
+	},
+	{
+		id: 7108,
+		attendance: 59996,
+		away_team: 2014,
+		away_goals: 2,
+		home_team: 2012,
+		home_goals: 2,
+		date: 1439127000000,
+		round: 1,
+		referee: 'Craig Pawson',
+		season: '2015\/2016',
+		venue: 6011,
+		type: 'Premiere League'
+	},
+	{
+		id: 7109,
+		attendance: 27654,
+		away_team: 2001,
+		away_goals: 1,
+		home_team: 2015,
+		home_goals: 0,
+		date: 1439136000000,
+		round: 1,
+		referee: 'Anthony Taylor',
+		season: '2015\/2016',
+		venue: 6014,
+		type: 'Premiere League'
+	},
+	{
+		id: 7110,
+		attendance: 24564,
+		away_team: 2011,
+		away_goals: 3,
+		home_team: 2019,
+		home_goals: 0,
+		date: 1439236800000,
+		round: 1,
+		referee: 'Mike Dean',
+		season: '2015\/2016',
+		venue: 6019,
+		type: 'Premiere League'
+	},
+	{
+		id: 7111,
+		attendance: 42200,
+		away_team: 2002,
+		away_goals: 1,
+		home_team: 2006,
+		home_goals: 0,
+		date: 1439581500000,
+		round: 2,
+		referee: 'Mike Dean',
+		season: '2015\/2016',
+		venue: 6003,
+		type: 'Premiere League'
+	},
+	{
+		id: 7112,
+		attendance: 36004,
+		away_team: 2015,
+		away_goals: 2,
+		home_team: 2003,
+		home_goals: 2,
+		date: 1439650800000,
+		round: 2,
+		referee: 'Robert Madley',
+		season: '2015\/2016',
+		venue: 6017,
+		type: 'Premiere League'
+	}
+];
+
+window.players = [
+	{
+		id: 1003,
+		name: 'Philippe Coutinho',
+		team: 1004
+	},
+	{
+		id: 1006,
+		name: 'Marouane Fellaini',
+		team: 1005
+	}
+];
+
+window.teams = [
+	{
+		id: 2001,
+		logo: 'http://www.premierleague.com/content/dam/premierleague/shared-images/clubs/l/liverpool/logo.png/_jcr_content/renditions/cq5dam.thumbnail.140.100.png',
+		club: 'Liverpool FC'
+	},
+	{
+		id: 2002,
+		logo: 'http://www.premierleague.com/content/dam/premierleague/shared-images/clubs/m/man-utd/logo.png/_jcr_content/renditions/cq5dam.thumbnail.140.100.png',
+		club: 'Manchester United'
+	},
+	{
+		id: 2003,
+		logo: 'http://www.premierleague.com/content/dam/premierleague/shared-images/clubs/s/spurs/logo.png/_jcr_content/renditions/cq5dam.thumbnail.140.100.png',
+		club: 'Tottanham Hotspurs'
+	},
+	{
+		id: 2004,
+		logo: 'http://www.premierleague.com/content/dam/premierleague/shared-images/clubs/b/bournemouth/logo.png/_jcr_content/renditions/cq5dam.thumbnail.140.100.png',
+		club: 'AFC Bournemouth'
+	},
+	{
+		id: 2005,
+		logo: 'http://www.premierleague.com/content/dam/premierleague/shared-images/clubs/a/arsenal/logo.png/_jcr_content/renditions/cq5dam.thumbnail.140.100.png',
+		club: 'Arsenal'
+	},
+	{
+		id: 2006,
+		logo: 'http://www.premierleague.com/content/dam/premierleague/shared-images/clubs/a/aston-villa/logo.png/_jcr_content/renditions/cq5dam.thumbnail.140.100.png',
+		club: 'Aston Villa'
+	},
+	{
+		id: 2007,
+		logo: 'http://www.premierleague.com/content/dam/premierleague/shared-images/clubs/c/chelsea/logo.png/_jcr_content/renditions/cq5dam.thumbnail.140.100.png',
+		club: 'Chelsea'
+	},
+	{
+		id: 2008,
+		logo: 'http://www.premierleague.com/content/dam/premierleague/shared-images/clubs/c/crystal-palace/logo.png/_jcr_content/renditions/cq5dam.thumbnail.140.100.png',
+		club: 'Crystal Palace'
+	},
+	{
+		id: 2009,
+		logo: 'http://www.premierleague.com/content/dam/premierleague/shared-images/clubs/e/everton/logo.png/_jcr_content/renditions/cq5dam.thumbnail.140.100.png',
+		club: 'Everton'
+	},
+	{
+		id: 2010,
+		logo: 'http://www.premierleague.com/content/dam/premierleague/shared-images/clubs/l/leicester/logo.png/_jcr_content/renditions/cq5dam.thumbnail.140.100.png',
+		club: 'Leicester City'
+	},
+	{
+		id: 2011,
+		logo: 'http://www.premierleague.com/content/dam/premierleague/shared-images/clubs/m/man-city/logo.png/_jcr_content/renditions/cq5dam.thumbnail.140.100.png',
+		club: 'Manchester City'
+	},
+	{
+		id: 2012,
+		logo: 'http://www.premierleague.com/content/dam/premierleague/shared-images/clubs/n/newcastle/logo.png/_jcr_content/renditions/cq5dam.thumbnail.140.100.png',
+		club: 'Newcastle United'
+	},
+	{
+		id: 2013,
+		logo: 'http://www.premierleague.com/content/dam/premierleague/shared-images/clubs/n/norwich/logo.png/_jcr_content/renditions/cq5dam.thumbnail.140.100.png',
+		club: 'Norwich City'
+	},
+	{
+		id: 2014,
+		logo: 'http://www.premierleague.com/content/dam/premierleague/shared-images/clubs/s/southampton/logo.png/_jcr_content/renditions/cq5dam.thumbnail.140.100.png',
+		club: 'Southampton'
+	},
+	{
+		id: 2015,
+		logo: 'http://www.premierleague.com/content/dam/premierleague/shared-images/clubs/s/stoke/logo.png/_jcr_content/renditions/cq5dam.thumbnail.140.100.png',
+		club: 'Stoke City'
+	},
+	{
+		id: 2016,
+		logo: 'http://www.premierleague.com/content/dam/premierleague/shared-images/clubs/s/sunderland/logo.png/_jcr_content/renditions/cq5dam.thumbnail.140.100.png',
+		club: 'Sunderland AFC'
+	},
+	{
+		id: 2017,
+		logo: 'http://www.premierleague.com/content/dam/premierleague/shared-images/clubs/s/swansea/logo.png/_jcr_content/renditions/cq5dam.thumbnail.140.100.png',
+		club: 'Swansea City'
+	},
+	{
+		id: 2018,
+		logo: 'http://www.premierleague.com/content/dam/premierleague/shared-images/clubs/w/watford/logo.png/_jcr_content/renditions/cq5dam.thumbnail.140.100.png',
+		club: 'Watford FC'
+	},
+	{
+		id: 2019,
+		logo: 'http://www.premierleague.com/content/dam/premierleague/shared-images/clubs/w/west-brom/logo.png/_jcr_content/renditions/cq5dam.thumbnail.140.100.png',
+		club: 'West Brom'
+	},
+	{
+		id: 2020,
+		logo: 'http://www.premierleague.com/content/dam/premierleague/shared-images/clubs/w/west-ham/logo.png/_jcr_content/renditions/cq5dam.thumbnail.140.100.png',
+		club: 'West Ham'
+	}
+];
+
+window.venues = [
+	{
+		id: 6001,
+		built: 1909,
+		country: 'UK',
+		name: 'Old Trafford',
+		city: 'Manchester',
+		capacity: 75653
+	},
+	{
+		id: 6002,
+		built: 2006,
+		country: 'UK',
+		name: 'Emirates',
+		city: 'London',
+		capacity: 60260
+	},
+	{
+		id: 6003,
+		built: 1897,
+		country: 'UK',
+		name: 'Villa Park',
+		city: 'Birmingham',
+		capacity: 42660
+	},
+	{
+		id: 6004,
+		built: 1910,
+		country: 'UK',
+		name: 'Vitality',
+		city: 'Bournemouth',
+		capacity: 11464
+	},
+	{
+		id: 6005,
+		build: 1877,
+		name: 'Stamford Bridge',
+		city: 'London',
+		capacity: 41798
+	},
+	{
+		id: 6006,
+		built: 1924,
+		name: 'Selhurst Park',
+		city: 'London',
+		capacity: 25073
+	},
+	{
+		id: 6007,
+		built: 1892,
+		name: 'Goodison Park',
+		city: 'Liverpool',
+		capacity: 39571
+	},
+	{
+		id: 6008,
+		built: 2002,
+		name: 'King Power',
+		city: 'Leicester',
+		capacity: 32312 
+	},
+	{
+		id: 6009,
+		built: 1884,
+		name: 'Anfield',
+		city: 'Liverpool',
+		capacity: 44742
+	},
+	{
+		id: 60010,
+		built: 2002,
+		name: 'Etihad',
+		city: 'Manchester',
+		capacity: 55097
+	},
+	{
+		id: 6011,
+		built: 1892,
+		name: "St James' Park",
+		city: 'Newcastle',
+		capacity: 52338
+	},
+	{
+		id: 6012,
+		built: 1935,
+		name: 'Carrow Road',
+		city: 'Norwich',
+		capacity: 27010
+	},
+	{
+		id: 6013,
+		built: 2001,
+		name: "St. Mary's",
+		city: 'Southampton',
+		capacity: 32505
+	},
+	{
+		id: 6014,
+		built: 1997,
+		name: 'Britannia',
+		city: 'Stoke-on-Trent',
+		capacity: 27740
+	},
+	{
+		id: 6015,
+		built: 1997,
+		name: 'Stadium of Light',
+		city: 'Sunderland',
+		capacity: 48707
+	},
+	{
+		id: 6016,
+		built: 2005,
+		name: 'Liberty Stadium',
+		city: 'Swansea',
+		capacity: 20909
+	},
+	{
+		id: 6017,
+		built: 1889,
+		name: 'White Hart Lane',
+		city: 'London',
+		capacity: 36284
+	},
+	{
+		id: 6018,
+		built: 1922,
+		name: 'Vicarage Road Stadium',
+		city: 'Watford',
+		capacity: 21500
+	},
+	{
+		id: 6019,
+		built: 1900,
+		name: 'The Hawthorns',
+		city: 'West Midlands',
+		capacity: 26850
+	},
+	{
+		id: 6020,
+		built: 1904,
+		name: 'Boleyn Ground',
+		city: 'London',
+		capacity: 35345
+	}
+];
